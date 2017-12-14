ace3e368f16d03591a9de381a11a0e319466c691
Bryan Perez <bryanprz00@gmail.com>
2017-12-14 13:43:00 -0500

    Adding form styles to searchbar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -7,35 +7,126 @@ import '../style/search_bar.css';
 class SearchBar extends Component {
   constructor(props) {
   	super(props);
-  	this.state = { term: '' };
+  	this.state = { address: '' };
   	this.onInputChange = this.onInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onCheckboxSelect = this.onCheckboxSelect.bind(this);
   }
 
   onInputChange(event) {
-  	this.setState({ term: event.target.value });
+  	this.setState({ address: event.target.value });
   }
 
   onFormSubmit(event) {
   	event.preventDefault();
-    this.props.addLocation(this.state.term); // sending to actioncreator
-    this.setState({ term: '' });
+    this.props.addLocation(this.state.address); // sending to actioncreator
+    this.setState({ address: '' });
+  }
+
+  onCheckboxSelect(event) {
+    console.log("checkbox selected:", event);
   }
 
   render() {
   	return (
       <div className="col-md-4" >
-        <form className="input-group" onSubmit={this.onFormSubmit}>
-          <input 
-            type="search" 
-            value={this.state.term}
-            placeholder="Agregue una nueva direccion al mapa"
-            className="form-control"
-            onChange={this.onInputChange}
-          />
-          <span className="input-group-btn">
-            <button className="btn btn-secondary" type="submit">Buscar</button>
-          </span>
+        <h5>Agregue un nuevo lugar al mapa</h5>
+        <form onSubmit={this.onFormSubmit}>
+          <div className="form-group">
+            <input 
+              id="address-input"
+              type="search" 
+              value={this.state.address}
+              placeholder="Direccion de nuevo lugar"
+              className="form-control"
+              onChange={this.onInputChange}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-1"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 1" 
+                className="form-check-input"
+              />
+                <label for="value-1" className="form-check-label">Value 1</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-2"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 2" 
+                className="form-check-input"
+              />
+              <label for="value-2" className="form-check-label">Value 2</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-3"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 3" 
+                className="form-check-input"
+              />
+              <label for="value-3" className="form-check-label">Value 3</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-4"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 4" 
+                className="form-check-input"
+              />
+              <label for="value-4" className="form-check-label">Value 4</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-5"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 5" 
+                className="form-check-input"
+              />
+              <label for="value-5" className="form-check-label">Value 5</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-6"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 6" 
+                className="form-check-input"
+              />
+              <label for="value-6" className="form-check-label">Value 6</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="form-check">
+              <input 
+                id="value-7"
+                type="checkbox" 
+                onChange={this.onCheckboxSelect} 
+                value="value 7" 
+                className="form-check-input"
+              />
+              <label for="value-7" className="form-check-label">Value 7</label>
+            </div>
+          </div>
+          <button className="btn btn-secondary" type="submit">Agregue Nuevo Lugar</button>
         </form>
       </div>
   	)

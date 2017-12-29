34e91c21a3965010c20835f5a7af6e3e328d9149
Bryan Perez <bryanprz00@gmail.com>
2017-12-29 18:29:12 -0500

    Push new locations to Firebase

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,39 @@
 import axios from 'axios';
+import * as firebase from 'firebase';
 
 export const ADD_LOCATION = 'ADD_LOCATION';
+const MAP_ROOT_URL = `https://maps.googleapis.com/maps/api/geocode/json`;
 
-const ROOT_URL = `https://maps.googleapis.com/maps/api/geocode/json`;
+var config = {
+  apiKey: "AIzaSyC4UDfcaOGCd6rZpCp4EgB4oJljXEDBh6g",
+  authDomain: "mapeo-e6a07.firebaseapp.com",
+  databaseURL: "https://mapeo-e6a07.firebaseio.com",
+  projectId: "mapeo-e6a07",
+  storageBucket: "mapeo-e6a07.appspot.com",
+  messagingSenderId: "501082432840"
+};
+
+firebase.initializeApp(config);
+
+export function fetchLocations() {
+  firebase.database().ref().on('value', snapshot => {
+    console.log('database ref: ', snapshot.val());
+  });
+  return dispatch => {
+    //Locations.on('value', snapshot => {
+      //console.log('snapshot: ', snapshot);
+      //dispatch({
+        //type: ADD_LOCATION,
+        //payload: snapshot.val()
+      //});
+    //});
+  }
+}
 
 export function addLocation(values = {}) {
-  const url = `${ROOT_URL}?address=${values.address}`;
+  firebase.database().ref().child('locations').push(values);
+
+  const url = `${MAP_ROOT_URL}?address=${values.address}`;
   values.request = axios.get(url);
 
   return { 
